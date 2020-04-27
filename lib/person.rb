350e569e23a26a6f29be7525179147752dad1052
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-27 21:11:58 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,6 @@
-class Person
+class Person #defining the dog class with the keyword class
 end
 
+#The instances below are brought to life by adding person.new to the variables representing the names of a person
 adele_goldberg = Person.new
 alan_kay = Person.new
